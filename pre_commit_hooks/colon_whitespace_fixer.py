e19ab43f2333a38654f51bf163af3788fa153e07
Jingjing Peng <jingjing@yelp.com>
2016-07-22 15:26:37 -0700

    update

--- pre_commit_hooks/colon_whitespace_fixer.py
+++ pre_commit_hooks/colon_whitespace_fixer.py
@@ -9,25 +9,66 @@ import re
 from pre_commit_hooks.util import cmd_output
 
 
-def fix_file(filename):
+def _fix_file(filename):
     for line in fileinput.input([filename], inplace=True):
         # preserve trailing two-space for non-blank lines in markdown files
             #if 'where' in line:
                 # if there is space btw : and var name
                 line = re.sub(r':\s+', ':', line)
-    print(line.rstrip())
+    print(line)
 
-def main(argv=None):
+def fix_colon_whitespace(argv=None):
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--no-markdown-linebreak-ext',
+        action='store_const',
+        const=[],
+        default=argparse.SUPPRESS,
+        dest='markdown_linebreak_ext',
+        help='Do not preserve linebreak spaces in Markdown'
+    )
+    parser.add_argument(
+        '--markdown-linebreak-ext',
+        action='append',
+        const='',
+        default=['md,markdown'],
+        metavar='*|EXT[,EXT,...]',
+        nargs='?',
+        help='Markdown extensions (or *) for linebreak spaces'
+    )
     parser.add_argument('filenames', nargs='*', help='Filenames to fix')
     args = parser.parse_args(argv)
 
-    ret = 0
+    bad_whitespace_files = cmd_output(
+        'grep', '-l', '[[:space:]]$', *args.filenames, retcode=None
+    ).strip().splitlines()
 
-    for filename in args.filenames:
-        file = fix_file(filename)
-        if file != 0:
-            print('Fixing strings in {0}'.format(filename))
-        ret |= file
+    md_args = args.markdown_linebreak_ext
+    if '' in md_args:
+        parser.error('--markdown-linebreak-ext requires a non-empty argument')
+    all_markdown = '*' in md_args
+    # normalize all extensions; split at ',', lowercase, and force 1 leading '.'
+    md_exts = ['.' + x.lower().lstrip('.')
+               for x in ','.join(md_args).split(',')]
 
-    return ret
+    # reject probable "eaten" filename as extension (skip leading '.' with [1:])
+    for ext in md_exts:
+        if any(c in ext[1:] for c in r'./\:'):
+            parser.error(
+                "bad --markdown-linebreak-ext extension '{0}' (has . / \\ :)\n"
+                "  (probably filename; use '--markdown-linebreak-ext=EXT')"
+                .format(ext)
+            )
+
+    if bad_whitespace_files:
+        for bad_whitespace_file in bad_whitespace_files:
+            print('Fixing {0}'.format(bad_whitespace_file))
+            _, extension = os.path.splitext(bad_whitespace_file.lower())
+            _fix_file(bad_whitespace_file, all_markdown or extension in md_exts)
+        return 1
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    sys.exit(fix_colon_whitespace())
